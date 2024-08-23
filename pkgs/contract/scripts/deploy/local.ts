d7fdef7830119d84c2793b7aaf56c472df5ad9dc
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-08-24 08:44:17 +0900

    split creator base

--- pkgs/contract/scripts/deploy/local.ts
+++ pkgs/contract/scripts/deploy/local.ts
@@ -1,16 +1,21 @@
 import {deployHatsProtocol} from "../modules/Hats";
-import {deploySplitsProtocol} from "../modules/Splits";
+import {deploySplitCreator, deploySplitsProtocol} from "../modules/Splits";
 
 const main = async () => {
   const {SplitsWarehouse, PullSplitsFactory, PushSplitsFactory} =
     await deploySplitsProtocol();
   const {Hats} = await deployHatsProtocol();
 
+  const {SplitCreator} = await deploySplitCreator(
+    await PullSplitsFactory.getAddress()
+  );
+
   console.log("------ Deployed contracts ------");
   console.log("SplitsWarehouse: ", await SplitsWarehouse.getAddress());
   console.log("PullSplitsFactory: ", await PullSplitsFactory.getAddress());
   console.log("PushSplitsFactory: ", await PushSplitsFactory.getAddress());
   console.log("Hats: ", await Hats.getAddress());
+  console.log("SplitCreator: ", await SplitCreator.getAddress());
 };
 
 main();

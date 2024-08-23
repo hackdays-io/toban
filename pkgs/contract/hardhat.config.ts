8a422d91c0fd967772ad3f24182162690fce56fd
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-08-23 21:00:32 +0900

    tmp

--- pkgs/contract/hardhat.config.ts
+++ pkgs/contract/hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          viaIR: true,
+        },
+      },
+    ]
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    }
+  }
 };
 
 export default config;
---
+++ pkgs/contract/ignition/modules/Hats.ts
@@ -0,0 +1,11 @@
+import { ethers } from "hardhat"
+
+export const deployHatsProtocol = async () => {
+  const factory = await ethers.getContractFactory("Hats")
+  const Hats = await factory.deploy("test", "https://test.com")
+  await Hats.waitForDeployment()
+
+  return { Hats }
+}
+
+export const mintTopHat = async () => {}

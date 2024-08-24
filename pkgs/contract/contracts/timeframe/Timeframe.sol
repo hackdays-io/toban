32f0250840fa103cbd9b6894b5097b2a81a54339
k.ikemizu <k.ikemizu@questry.co.jp>
2024-08-24 11:42:55 +0900

    wip: timeframe.sol

---
+++ pkgs/contract/contracts/timeframe/Timeframe.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+
+contract Timeframe is ERC2771Context {
+    // Mapping from address and hatId to the timestamp when the hat was minted
+    mapping(address => mapping(uint256 => uint256)) private woreTime;
+
+    /**
+     * @dev Constructor to initialize the trusted forwarder.
+     * @param _trustedForwarder Address of the trusted forwarder contract.
+     */
+    constructor(address _trustedForwarder) ERC2771Context(_trustedForwarder) {}
+
+    /**
+     * @dev Sets the timestamp when a specific hat was minted for a specific address.
+     * Can only be called by the contract that handles the minting logic.
+     * @param hatId The ID of the hat that was minted.
+     */
+    function setWoreTime(uint256 hatId) external {
+        address wearer = _msgSender();
+        require(woreTime[wearer][hatId] == 0, "Hat already minted");
+        woreTime[wearer][hatId] = block.timestamp;
+    }
+
+    /**
+     * @dev Gets the timestamp when a specific hat was minted for a specific address.
+     * @param wearer The address of the person who received the hat.
+     * @param hatId The ID of the hat that was minted.
+     * @return The timestamp when the hat was minted.
+     */
+    function getWoreTime(address wearer, uint256 hatId) external view returns (uint256) {
+        return woreTime[wearer][hatId];
+    }
+
+    /**
+     * @dev Gets the elapsed time in seconds since the specific hat was minted for a specific address.
+     * @param wearer The address of the person who received the hat.
+     * @param hatId The ID of the hat that was minted.
+     * @return The elapsed time in seconds.
+     */
+    function getWearingElapsedTime(address wearer, uint256 hatId) external view returns (uint256) {
+        uint256 mintTime = woreTime[wearer][hatId];
+        require(mintTime != 0, "Hat has not been minted for this wearer and hatId");
+        return block.timestamp - mintTime;
+    }
+
+    /**
+     * @dev Override _msgSender to use the context from ERC2771Context.
+     * @return The message sender address.
+     */
+    function _msgSender() internal view override(ERC2771Context) returns (address) {
+        return ERC2771Context._msgSender();
+    }
+
+    /**
+     * @dev Override _msgData to use the context from ERC2771Context.
+     * @return The calldata of the message.
+     */
+    function _msgData() internal view override(ERC2771Context) returns (bytes calldata) {
+        return ERC2771Context._msgData();
+    }
+}

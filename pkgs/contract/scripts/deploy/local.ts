f40becd2265a267114a47ac77bb6eb15c7430bc0
k.ikemizu <k.ikemizu@questry.co.jp>
2024-08-24 19:40:17 +0900

    chore

--- pkgs/contract/scripts/deploy/local.ts
+++ pkgs/contract/scripts/deploy/local.ts
@@ -1,13 +1,28 @@
+import {ZeroAddress} from "ethers";
+import {deployFractionToken} from "../modules/FractionToken";
 import {deployHatsProtocol} from "../modules/Hats";
 import {deploySplitCreator, deploySplitsProtocol} from "../modules/Splits";
 
 const main = async () => {
-  const {SplitsWarehouse, PullSplitsFactory, PushSplitsFactory} =
-    await deploySplitsProtocol();
+  const {
+    SplitsWarehouse,
+    PullSplitsFactory,
+    PushSplitsFactory,
+    SampleForwarder,
+  } = await deploySplitsProtocol();
   const {Hats} = await deployHatsProtocol();
 
+  const {FractionToken: _FractionToken} = await deployFractionToken(
+    "",
+    await Hats.getAddress(),
+    ZeroAddress
+  );
+  let FractionToken = _FractionToken;
+
   const {SplitCreator} = await deploySplitCreator(
-    await PullSplitsFactory.getAddress()
+    await PullSplitsFactory.getAddress(),
+    await FractionToken.getAddress(),
+    await SampleForwarder.getAddress()
   );
 
   console.log("------ Deployed contracts ------");

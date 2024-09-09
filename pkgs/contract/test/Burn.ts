3256a94b8a3e45eded70a91f70d1845cf4e2710f
aowheel <aoimakino2003@gmail.com>
2024-09-09 20:05:24 +0900

    burn関数とそのテストコードを追加しました

--- pkgs/contract/test/Burn.ts
+++ pkgs/contract/test/Burn.ts
@@ -2,6 +2,7 @@ import { decodeEventLog, PublicClient, WalletClient, zeroAddress } from "viem";
 import { deployFractionToken, FractionToken } from "../helpers/deploy/FractionToken";
 import { deployHatsProtocol, Hats } from "../helpers/deploy/Hats";
 import { viem } from "hardhat";
+import { expect } from "chai";
 
 describe("Burn", () => {
   let Hats: Hats;
@@ -11,8 +12,7 @@ describe("Burn", () => {
   let address2: WalletClient;
   let address3: WalletClient;
 
-  let hat1_id: bigint;
-  let hat2_id: bigint;
+  let hatId: bigint;
 
   let publicClient: PublicClient;
 
@@ -61,47 +61,20 @@ describe("Burn", () => {
 					data: log.data,
 					topics: log.topics,
 				});
-				if (decodedLog.eventName == "HatCreated") hat1_id = decodedLog.args.id;
-			} catch (error) {}
-		}
-
-		txHash = await Hats.write.createHat([
-			BigInt(
-				"0x0000000100000000000000000000000000000000000000000000000000000000"
-			),
-			"role2",
-			10,
-			"0x0000000000000000000000000000000000004a75",
-			"0x0000000000000000000000000000000000004a75",
-			true,
-			"https://test.com/hat_image.png",
-		]);
-
-		receipt = await publicClient.waitForTransactionReceipt({
-			hash: txHash,
-		});
-
-		for (const log of receipt.logs) {
-			try {
-				const decodedLog = decodeEventLog({
-					abi: Hats.abi,
-					data: log.data,
-					topics: log.topics,
-				});
-				if (decodedLog.eventName == "HatCreated") hat2_id = decodedLog.args.id;
+				if (decodedLog.eventName == "HatCreated") hatId = decodedLog.args.id;
 			} catch (error) {}
 		}
 
     // address1とaddress2にHatをmint
-		await Hats.write.mintHat([hat1_id, address1.account?.address!]);
-		await Hats.write.mintHat([hat2_id, address2.account?.address!]);
+		await Hats.write.mintHat([hatId, address1.account?.address!]);
+		await Hats.write.mintHat([hatId, address2.account?.address!]);
 
 		// address1とaddress2にFractionTokenをmint
-		await FractionToken.write.mint([hat1_id, address1.account?.address!]);
-		await FractionToken.write.mint([hat2_id, address2.account?.address!]);
+		await FractionToken.write.mint([hatId, address1.account?.address!]);
+		await FractionToken.write.mint([hatId, address2.account?.address!]);
 
     const tokenId = await FractionToken.read.getTokenId([
-			hat2_id,
+			hatId,
 			address2.account?.address!,
 		]);
 
@@ -118,28 +91,65 @@ describe("Burn", () => {
 				account: address2.account!,
 			}
 		);
-
-    it("should burn tokens", async () => {
-      await FractionToken.write.burn(
-        [
-          address1.account?.address!,
-          address2.account?.address!,
-          hat1_id,
-          10000n
-        ],
-        {
-          account: address1.account!
-        }
-      );
-
-      // このbalanceがnever型になってしまいます
-      const balance = await FractionToken.read.balanceOf(
-        [
-          address1.account?.address!,
-          address2.account?.address!,
-          hat1_id
-        ]
-      );
-    })
   });
-})
+
+  it("should burn tokens", async () => {
+		// address1のtokenを自身で半分burnする
+    await FractionToken.write.burn(
+      [
+        address1.account?.address!,
+        address1.account?.address!,
+        hatId,
+        5000n
+      ],
+      {
+        account: address1.account!
+      }
+    );
+
+		// address3のtokenをaddress1によってすべてburnする
+		await FractionToken.write.burn(
+      [
+        address3.account?.address!,
+        address2.account?.address!,
+        hatId,
+        5000n
+      ],
+      {
+        account: address1.account!
+      }
+    );
+
+		let balance: BigInt;
+
+		// address1のbalance
+    balance = await FractionToken.read.balanceOf(
+      [
+        address1.account?.address!,
+        address1.account?.address!,
+        hatId
+      ]
+    );
+		expect(balance).to.equal(5000n);
+
+		// address2のbalance
+		balance = await FractionToken.read.balanceOf(
+      [
+        address2.account?.address!,
+        address2.account?.address!,
+        hatId
+      ]
+    );
+		expect(balance).to.equal(5000n);
+
+		// address3のbalance
+		balance = await FractionToken.read.balanceOf(
+      [
+        address3.account?.address!,
+        address2.account?.address!,
+        hatId
+      ]
+    );
+		expect(balance).to.equal(0n);
+  });
+});

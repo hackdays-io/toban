4febef959c7d4a2520578fc4c6db83dbdb3bddc0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-09-16 20:29:41 +0900

    新しくサブドメインを登録するためのタスクファイルとそのためのヘルパーメソッドを追加しました。

--- pkgs/contract/hardhat.config.ts
+++ pkgs/contract/hardhat.config.ts
@@ -19,7 +19,7 @@ const {
 // タスクファイルを読み込むための設定
 const SKIP_LOAD = process.env.SKIP_LOAD === "true";
 if (!SKIP_LOAD) {
-	const taskPaths = ["", "utils"];
+	const taskPaths = ["", "utils", "ens"];
 	taskPaths.forEach((folder) => {
 		const tasksPath = path.join(__dirname, "tasks", folder);
 		fs.readdirSync(tasksPath)

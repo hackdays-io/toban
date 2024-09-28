13ef885a73e93860abb8d1c54f640d93e33103b8
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-09-28 13:32:30 +0900

    sol

---
+++ pkgs/contract/contracts/bigbang/BigBang.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { IHats } from "../hats/src/Interfaces/IHats.sol";
+import { IHatsModuleFactory } from "./IHatsModuleFactory.sol";
+import { ISplitsCreatorFactory } from "../splitscreator/ISplitsCreatorFactory.sol";
+import { HatsTimeFrameModule } from "../timeframe/HatsTimeFrameModule.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+
+contract BigBang is ERC2771Context {
+	IHats public Hats;
+
+	IHatsModuleFactory public HatsModuleFactory;
+
+	ISplitsCreatorFactory public SplitsCreatorFactory;
+
+	address public HatsTimeFrameModule_IMPL;
+
+	event BigBang(
+		address indexed owner,
+		uint256 indexed topHatId,
+		address hatsTimeFrameModule,
+		address splitCreator
+	);
+
+	/**
+	 * @dev Constructor to initialize the trusted forwarder.
+	 * @param _trustedForwarder Address of the trusted forwarder contract.
+	 * @param _hatsAddress Address of the hats protocol V1 contract.
+	 */
+	constructor(
+		address _trustedForwarder,
+		address _hatsAddress,
+		address _hatsModuleFactory,
+		address _hatsTimeFrameModule_IMPL,
+		address _splitsCreatorFactory
+	) ERC2771Context(_trustedForwarder) {
+		Hats = IHats(_hatsAddress);
+		HatsModuleFactory = IHatsModuleFactory(_hatsModuleFactory);
+		HatsTimeFrameModule_IMPL = _hatsTimeFrameModule_IMPL;
+		SplitsCreatorFactory = ISplitsCreatorFactory(_splitsCreatorFactory);
+	}
+
+	/**
+	 * @dev
+	 * @param _owner The address of the user who will own the topHat.
+	 * @param _topHatDetails The details of the topHat.
+	 * @param _topHatImageURI The image URI of the topHat.
+	 * @param _hatterHatDetails The details of the hatterHat.
+	 * @param _hatterHatImageURI The image URI of the hatterHat.
+	 * @param _trustedForwarder The address of the trusted forwarder.
+	 * @param _splitFactoryV2 The address of the splitFactoryV2 contract.
+	 * @param _fractionToken The address of the fractionToken contract.
+	 * @return topHatId The ID used for navigating to the ProjectTop page after project creation.
+	 */
+	function bigbang(
+		address _owner,
+		string calldata _topHatDetails,
+		string calldata _topHatImageURI,
+		string calldata _hatterHatDetails,
+		string calldata _hatterHatImageURI,
+		address _trustedForwarder,
+		address _splitFactoryV2,
+		address _fractionToken
+	) external returns (uint256) {
+		// 1. TopHatのMint
+
+		uint256 topHatId = Hats.mintTopHat(
+			address(this), // target: Tophat's wearer address. topHatのみがHatterHatを作成できるためTophatを指定する
+			_topHatDetails,
+			_topHatImageURI
+		);
+
+		// 2. HatterHatの作成
+
+		uint256 hatterHatId = Hats.createHat(
+			topHatId, // _admin: The id of the Hat that will control who wears the newly created hat
+			_hatterHatDetails,
+			1,
+			0x0000000000000000000000000000000000004A75,
+			0x0000000000000000000000000000000000004A75,
+			true,
+			_hatterHatImageURI
+		);
+
+		// 4. HatsTimeFrameModuleのデプロイ
+		address hatsTimeFrameModule = HatsModuleFactory.createHatsModule(
+			HatsTimeFrameModule_IMPL,
+			topHatId,
+			"",
+			"",
+			0
+		);
+
+		// 5. HatsTimeFrameModuleにHatterHatをMint
+		Hats.mintHat(hatterHatId, hatsTimeFrameModule);
+
+		// 6. TopHatIdの権限を_ownerに譲渡
+		Hats.transferHat(topHatId, address(this), _owner);
+
+		// 7. SplitCreatorをFactoryからデプロイ
+		address splitCreator = SplitsCreatorFactory
+			.createSplitCreatorDeterministic(
+				topHatId,
+				_trustedForwarder,
+				_splitFactoryV2,
+				hatsTimeFrameModule,
+				_fractionToken,
+				keccak256(abi.encodePacked(topHatId))
+			);
+
+		emit BigBang(_owner, topHatId, hatsTimeFrameModule, splitCreator);
+
+		return topHatId;
+	}
+
+	// Override _msgSender to use the context from ERC2771Context.
+	function _msgSender()
+		internal
+		view
+		override(ERC2771Context)
+		returns (address)
+	{
+		return ERC2771Context._msgSender();
+	}
+
+	// Override _msgData to use the context from ERC2771Context.
+	function _msgData()
+		internal
+		view
+		override(ERC2771Context)
+		returns (bytes calldata)
+	{
+		return ERC2771Context._msgData();
+	}
+}

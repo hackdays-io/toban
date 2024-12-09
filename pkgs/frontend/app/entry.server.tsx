3ad9aed1879c802547ea7de805baa055b830799e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-12-09 21:48:16 +0900

    i18nの対応途中。とりあえずプッシュします。

--- pkgs/frontend/app/entry.server.tsx
+++ pkgs/frontend/app/entry.server.tsx
@@ -1,18 +1,40 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
+import { createInstance } from "i18next";
+import Backend from "i18next-fs-backend";
+import { resolve } from "node:path";
+import { I18nextProvider, initReactI18next } from "react-i18next";
 import { createEmotion } from "./emotion/emotion-server";
+import i18n from "./i18n";
+import i18next from "./i18next.server";
 
-const handleRequest = (
+const handleRequest = async (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) =>
+) => {
+  const instance = createInstance();
+  const lng = await i18next.getLocale(request);
+  const ns = i18next.getRouteNamespaces(remixContext as any);
+
+  await instance
+    .use(initReactI18next) // Tell our instance to use react-i18next
+    .use(Backend) // Setup our backend
+    .init({
+      ...i18n, // spread the configuration
+      lng, // The locale we detected above
+      ns, // The namespaces the routes about to render wants to use
+      backend: { loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json") },
+    });
+
   new Promise((resolve) => {
     const { renderToString, injectStyles } = createEmotion();
 
     const html = renderToString(
-      <RemixServer context={remixContext} url={request.url} />
+      <I18nextProvider i18n={instance}>
+        <RemixServer context={remixContext} url={request.url} />
+      </I18nextProvider>
     );
 
     responseHeaders.set("Content-Type", "text/html");
@@ -24,5 +46,6 @@ const handleRequest = (
 
     resolve(response);
   });
+};
 
 export default handleRequest;
--- pkgs/frontend/app/root.tsx
+++ pkgs/frontend/app/root.tsx
@@ -1,26 +1,53 @@
 import { withEmotionCache } from "@emotion/react";
 import {
+  json,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 // import { ThemeProvider } from "next-themes";  // DarkMode 切り替えの実装の可能性に備え、ThemeProvider を残しておいてあります
-import { ChakraProvider } from "./components/chakra-provider";
-import { useInjectStyles } from "./emotion/emotion-client";
-import { PrivyProvider } from "@privy-io/react-auth";
 import { Box, Container } from "@chakra-ui/react";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useTranslation } from "react-i18next";
+import { useChangeLanguage } from "remix-i18next/react";
+import i18next from "~/i18next.server";
+import { ChakraProvider } from "./components/chakra-provider";
 import { Header } from "./components/Header";
+import { useInjectStyles } from "./emotion/emotion-client";
+
 interface LayoutProps extends React.PropsWithChildren {}
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const locale = await i18next.getLocale(request);
+  return json({ locale });
+}
+
+export const handle = {
+  // In the handle export, we can add a i18n key with namespaces our route
+  // will need to load. This key can be a single string or an array of strings.
+  // TIP: In most cases, you should set this to your defaultNS from your i18n config
+  // or if you did not set one, set it to the i18next default namespace "translation"
+  i18n: "common",
+};
+
+/**
+ * Layout
+ */
 export const Layout = withEmotionCache((props: LayoutProps, cache) => {
   const { children } = props;
+  // Get the locale from the loader
+  const { locale } = useLoaderData<typeof loader>();
+  const { i18n } = useTranslation();
 
   useInjectStyles(cache);
+  useChangeLanguage(locale);
 
   return (
-    <html lang="en">
+    <html lang={locale} dir={i18n.dir()}>
       <head suppressHydrationWarning>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />

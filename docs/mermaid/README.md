16ef83d0c9a750f700e3e02f66bd82c6eb293598
ikmz <ikmz0104.official@gmail.com>
2025-04-04 20:45:22 +0900

    Add notes on using Splits for post-game fund withdrawals

--- docs/mermaid/README.md
+++ docs/mermaid/README.md
@@ -1,2 +1,7 @@
 - mermaid好きな人
 - Javaインストールが面倒な人
+
+memo
+
+Splitsによる動作確認。
+`pnpm contract test test/Splits.ts`
--- pkgs/contract/test/Splits.ts
+++ pkgs/contract/test/Splits.ts
@@ -25,6 +25,11 @@ describe("Splits", () => {
   let address2Validated: Address;
   let publicClient: PublicClient;
 
+  // Push型： 送金者がすぐに受取人に送金(手数料も送金者負担)
+  // Pull型： 一度まとめて送金し、各受取人があとで「自分で取り出す」(宅急便の着払い、送料は受取人負担)
+
+  // 試合終了後にユーザが資金を引き出すフローに使えそう
+
   const getPullSplitContract = async (address: Address) => {
     return await viem.getContractAt("PullSplit", address);
   };
@@ -37,6 +42,9 @@ describe("Splits", () => {
   };
 
   before(async () => {
+
+    // 準備
+    // deploySplitsProtocol() を使って必要なコントラクトをデプロイ
     const {
       SplitsWarehouse: _SplitsWarehouse,
       PullSplitsFactory: _PullSplitsFactory,
@@ -98,7 +106,7 @@ describe("Splits", () => {
       throw new Error("address1 account is undefined");
     }
 
-    // Splitコントラクトに1ETH送金
+    // 1ETHをSplitコントラクトに送金 → distribute() → withdraw() → 残高が増えたか検証
     await address1.sendTransaction({
       account: address1.account,
       to: DeployedPullSplit.address,
@@ -110,27 +118,35 @@ describe("Splits", () => {
       address: address2Validated,
     });
 
+    // ETHをERC-20のように扱うための慣習的なダミーアドレス
+    const ETH_TOKEN_ADDRESS =
+      "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+
+    // Pull型の分配を実行
+    // 1ETHをaddress1とaddress2に50%ずつ分配
+    // 送信元（tx.from）はaddress1
     await DeployedPullSplit.write.distribute(
       [
         {
           recipients: [address1Validated, address2Validated],
           allocations: [50n, 50n],
           totalAllocation: 100n,
-          distributionIncentive: 0,
+          distributionIncentive: 0, // 実行者へのインセンティブ（=手数料）
         },
-        "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        address1Validated,
+        ETH_TOKEN_ADDRESS, //「分配対象トークン」のアドレス
+        address1Validated, // 分配トリガーを引く人（引数上の sender）
       ],
       {
         account: address1.account,
       },
     );
 
-    // withdrawを実行
+    // withdrawを実行し、各受取人が自分の報酬を引き出せるか
+    // address1 が「自分の ETH 報酬」を SplitsWarehouse から withdraw() してる。
     await SplitsWarehouse.write.withdraw(
       [address1Validated, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],
       {
-        account: address1.account,
+        account: address1.account, // msg.sender
       },
     );
 
@@ -146,6 +162,7 @@ describe("Splits", () => {
     });
 
     // withdrawのガス代を引いて大体0.5ETH増えているはず
+    // 宅急便の着払いみたいなイメージ
     expect(Number(afterAddress2Balance) - Number(beforeAddress2Balance)).gt(
       499900000000000000,
     );

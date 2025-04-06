4766ee1d608b6e71826dedc730a3f3639d892cd6
yu23ki14 <yuki_021423@yahoo.co.jp>
2025-04-06 21:55:18 +0900

    tmp

--- pkgs/frontend/app/routes/$treeId_.roles_.new.tsx
+++ pkgs/frontend/app/routes/$treeId_.roles_.new.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Grid, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { useNavigate, useParams } from "@remix-run/react";
 import { useHats } from "hooks/useHats";
 import { useCreateHatFromHatCreatorModule } from "hooks/useHatsHatCreatorModule";
@@ -18,10 +18,13 @@ import { BasicButton } from "~/components/BasicButton";
 import { ContentContainer } from "~/components/ContentContainer";
 import { PageHeader } from "~/components/PageHeader";
 import { RoleAttributesList } from "~/components/RoleAttributesList";
+import CommonButton from "~/components/common/CommonButton";
+import { CommonDialog } from "~/components/common/CommonDialog";
 import { InputDescription } from "~/components/input/InputDescription";
 import { InputImage } from "~/components/input/InputImage";
 import { InputName } from "~/components/input/InputName";
 import { AddRoleAttributeDialog } from "~/components/roleAttributeDialog/AddRoleAttributeDialog";
+import { DialogCloseTrigger } from "~/components/ui/dialog";
 
 const SectionHeading: FC<{ children: React.ReactNode }> = ({ children }) => (
   <Text mt={7}>{children}</Text>
@@ -33,6 +36,7 @@ const NewRole: FC = () => {
   const { uploadImageFileToIpfs, imageFile, setImageFile } =
     useUploadImageFileToIpfs();
 
+  const [imageLibId, setImageLibId] = useState<string | null>(null);
   const [roleName, setRoleName] = useState("");
   const [roleDescription, setRoleDescription] = useState("");
 
@@ -87,7 +91,10 @@ const NewRole: FC = () => {
       const parsedLog = await createHat({
         parentHatId: BigInt(hatterHatId),
         details: resUploadHatsDetails?.ipfsUri,
-        imageURI: resUploadImage?.ipfsUri || "",
+        imageURI:
+          resUploadImage?.ipfsUri ||
+          `ipfs://${imageLibrary.find((image) => image.id === imageLibId)?.cid}` ||
+          "",
       });
       const log = parsedLog?.find((log) => log.eventName === "HatCreated");
       if (!log) throw new Error("Failed to create hat transaction");
@@ -117,14 +124,94 @@ const NewRole: FC = () => {
     uploadImageFileToIpfs,
     wallet,
     getTreeInfo,
+    imageLibId,
   ]);
+
+  const imageLibrary = [
+    {
+      id: "rpg1",
+      url: "/images/imagelib/rpg1.png",
+      cid: "bafybeignhscpeh55e7y2iva3eliuqugmgve2hfkxb43yg72brdnlexutti",
+    },
+    {
+      id: "rpg2",
+      url: "/images/imagelib/rpg2.png",
+      cid: "bafybeibfdgmu525swbw644j5i4cnqkf5lxql24qwjc47ilctdvhkmue7ne",
+    },
+    {
+      id: "rpg3",
+      url: "/images/imagelib/rpg3.png",
+      cid: "bafybeigudaq4m4xpoocgisen6adfynuhcei2piuw3bs4zahvit7zcv3nuy",
+    },
+    {
+      id: "rpg4",
+      url: "/images/imagelib/rpg4.png",
+      cid: "bafybeicyvdsgjh4gxlzfqyrf47viq2cyr4dwswfjwcgcxipvalz2xqarni",
+    },
+  ];
 
   return (
     <>
       <Box w="100%">
         <PageHeader title="新しいロールを作成" />
         <ContentContainer>
-          <InputImage imageFile={imageFile} setImageFile={setImageFile} />
+          <Stack my="30px" gap={3}>
+            <InputImage
+              imageFile={
+                imageFile ||
+                imageLibrary.find((image) => image.id === imageLibId)?.url ||
+                null
+              }
+              setImageFile={setImageFile}
+            />
+            <CommonDialog
+              dialogTriggerReactNode={
+                <CommonButton size="xs" bgColor="blue.300">
+                  便利な素材から選択
+                </CommonButton>
+              }
+            >
+              <DialogCloseTrigger />
+              <Box p={4}>
+                <Heading fontSize="lg">役割画像ライブラリ</Heading>
+
+                <Grid
+                  gridTemplateColumns={"repeat(auto-fill, minmax(100px, 1fr))"}
+                  gap={4}
+                  mt={4}
+                >
+                  {imageLibrary.map((image) => (
+                    <Box
+                      key={`image${image.cid}`}
+                      onClick={() => setImageLibId(image.id)}
+                      position="relative"
+                    >
+                      <Image src={image.url} alt="" />
+                      {imageLibId === image.id && (
+                        <Box
+                          position="absolute"
+                          top={0}
+                          left={0}
+                          width="100%"
+                          height="100%"
+                          bgColor="blackAlpha.600"
+                          _before={{
+                            content: '"Selected"',
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            color: "white",
+                            fontSize: "lg",
+                          }}
+                        />
+                      )}
+                    </Box>
+                  ))}
+                </Grid>
+              </Box>
+            </CommonDialog>
+          </Stack>
           <InputName name={roleName} setName={setRoleName} />
           <InputDescription
             description={roleDescription}

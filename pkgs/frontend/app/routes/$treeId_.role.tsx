e5a2266ad206575cf9d4c340744c4775c1900487
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-08-31 13:48:54 +0900

    feat: Overhaul new role page layout for v2

--- pkgs/frontend/app/routes/$treeId_.role.tsx
+++ pkgs/frontend/app/routes/$treeId_.role.tsx
@@ -1,18 +1,33 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
-import { useParams } from "@remix-run/react";
+import {
+  AspectRatio,
+  Box,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Link, useNavigate, useParams } from "@remix-run/react";
 import { useGetBalanceOfFractionTokens } from "hooks/useFractionToken";
-import { useGetHats } from "hooks/useHats";
+import { useGetHats, useTreeInfo } from "hooks/useHats";
 import { useActiveWallet } from "hooks/useWallet";
 import { type FC, useMemo } from "react";
 import type { Address } from "viem";
 import { StickyNav } from "~/components/StickyNav";
 import { HatsListItemParser } from "~/components/common/HatsListItemParser";
 import RoleWithBalance from "~/components/roles/RoleWithBalance";
+import { MyRole } from "~/components/roles/MyRole";
+import { VRole } from "~/components/roles/VRole";
+import CommonButton from "~/components/common/CommonButton";
+import { FaPlus } from "react-icons/fa6";
 
 const WorkspaceWithBalance: FC = () => {
+  const navigate = useNavigate();
+
   const { treeId } = useParams();
+  const tree = useTreeInfo(Number(treeId));
 
   const { wallet } = useActiveWallet();
+  const me = wallet?.account?.address;
 
   const { data } = useGetBalanceOfFractionTokens({
     where: {
@@ -42,6 +57,54 @@ const WorkspaceWithBalance: FC = () => {
 
   return (
     <Box>
+      {/* All roles */}
+      <Box my={4}>
+        <Heading py={4}>当番一覧</Heading>
+        <SimpleGrid columns={4} gap={4}>
+          {tree?.hats
+            ?.filter((h) => Number(h.levelAtLocalTree) >= 2)
+            .map((h) => (
+              <Link key={`allrole${h.id}`} to={`/${treeId}/${h.id}`}>
+                <HatsListItemParser imageUri={h.imageUri} detailUri={h.details}>
+                  <VRole iconSize="80px" />
+                </HatsListItemParser>
+              </Link>
+            ))}
+          <VStack>
+            <AspectRatio width="80px" ratio={1}>
+              <CommonButton
+                rounded="xl"
+                onClick={() => navigate(`/${treeId}/roles/new`)}
+                bgColor="gray.300"
+              >
+                <FaPlus />
+              </CommonButton>
+            </AspectRatio>
+            <Text>Add role</Text>
+          </VStack>
+        </SimpleGrid>
+      </Box>
+
+      {/* My roles */}
+      <Box my={4}>
+        <Heading py={4}>担当当番</Heading>
+        <VStack gap={3} align="stretch">
+          {tree?.hats
+            ?.filter((h) => Number(h.levelAtLocalTree) >= 2)
+            .filter((h) => h.wearers?.some((w) => w.id === me?.toLowerCase()))
+            .map((h) => (
+              <HatsListItemParser
+                key={h.id}
+                imageUri={h.imageUri}
+                detailUri={h.details}
+              >
+                <MyRole address={me} treeId={treeId} hatId={h.id} />
+              </HatsListItemParser>
+            ))}
+        </VStack>
+      </Box>
+
+      {/* Role Share */}
       <Heading pb={5}>
         {["144", "175", "780"].includes(treeId || "")
           ? "ケアポイント"
